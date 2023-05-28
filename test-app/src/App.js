e1b2fa37cda0a9b216400ad999cbfb08b8bbab9d
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-05-28 17:46:07 +0800

    Добавлены тесты для проверки отрисовки страницы

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //Для теста асинхронщины
+    const [data, setData] = useState(null);
+    const [toggle, setToggle] = useState(false);
+
+    const onClick = () => {
+        setToggle(prevState => !prevState)
+    };
+
+    useEffect( () => {
+        const timeout = setTimeout( () => {
+            setData({});
+        }, 100);
+        return (() => clearTimeout(timeout))
+    }, []);
+    return (
+        <div className="App">
+            {toggle === true && <div data-testid='toggle-elem'>toggle</div>}
+            {data && <div style={{color: "red"}}>data</div>}
+            <h1>Hello world</h1>
+            <button data-testid='toggle-btn' onClick={onClick}>click me</button>
+            <input type="text" placeholder="input value ..." />
+        </div>
+    );
 }
 
 export default App;
--- test-app/src/App.test.js
+++ test-app/src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
+describe( 'Тестирование App', () => {
+    beforeEach(() => {
+        render(<App />);
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+    })
+    test('render react h1 element', () => {
+        const hElement = screen.getByText(/hello world/i);
+        expect(hElement).toBeInTheDocument();
+    });
+    test('render react button element', () => {
+        const buttonElement = screen.getByRole('button');
+        expect(buttonElement).toBeInTheDocument();
+    });
+    test('render react input element', () => {
+        const inputElement = screen.getByPlaceholderText(/input value/i);
+        expect(inputElement).toBeInTheDocument();
+        // screen.debug();
+    });
+    test('Проверка на отсутствие элемента на странице', () => {
+        const hello2World = screen.queryByText(/hello2/i);
+        expect(hello2World).toBeNull();
+    })
+    test('Проверка на aсинхронную отрисовку элемента', async () => {
+        screen.debug();
+        const data = await screen.findByText(/data/i);
+        expect(data).toBeInTheDocument();
+        screen.debug();
+    })
+    test('Проверка на стиль текста в элементе', async () => {
+        const data = await screen.findByText(/data/i);
+        expect(data).toHaveStyle({color: "red"});
+    })
+    test('Тестирование нажатия на кнопку и отрисовки по тогглу элемента', () => {
+        const btn = screen.getByTestId('toggle-btn');
+        expect(screen.queryByTestId('toggle-elem')).toBeNull();
+        fireEvent.click(btn);
+        expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();
+        fireEvent.click(btn);
+        expect(screen.queryByTestId('toggle-elem')).toBeNull();
+    })
+})
+
