052c3262c72cdea15756c1f014426eb929dc1f87
MiDimas <103483149+MiDimas@users.noreply.github.com>
2023-07-09 09:31:06 +0800

    Добавлен еще один тест на страницу hello

--- test-app/tests/e2e/hello.e2e.js
+++ test-app/tests/e2e/hello.e2e.js
@@ -8,4 +8,9 @@ describe("Hello tests", () => {
         await HelloPage.toggle.click();
         await expect(HelloPage.title).not.toBeExisting();
     })
+    it('Тестирование ввода некорректных данных и нажатия кнопки', async () => {
+        await HelloPage.open();
+        await HelloPage.toggleTittleWithInput('world');
+        await expect(HelloPage.title).not.toBeExisting();
+    })
 })
